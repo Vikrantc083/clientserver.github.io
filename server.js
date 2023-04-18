a263d609899f452b2820dac3b0f7e96fff0460c3
vikrantc672 <66821579+vikrantc672@users.noreply.github.com>
2023-04-18 22:23:22 +0530

    h

--- server.js
+++ server.js
@@ -10,18 +10,17 @@ const io = new Server(server);
 ur=uuidV4();
 app.get('/', (req, res) => {   
     
-    // res.redirect(`/${ur}`);
-    res.sendFile(__dirname + '/public/index.html');    
+    res.redirect(`/${ur}`);      
 
 });   
 app.use(express.static(__dirname + '/public'));
-// app.get('/:room', (req, res) => {
-//   res.render('room', { roomId: req.params.room });
-// });
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
 
-// app.get(`/${ur}`,(req,res)=>{  
-// res.sendFile(__dirname + '/public/index.html');
-// })
+app.get(`/${ur}`,(req,res)=>{  
+res.sendFile(__dirname + '/public/index.html');
+})
 io.on('connection', (socket) => {
   socket.on('join-room', (roomId, userId, use) => {
     console.log('joined room with id = ' + roomId);
